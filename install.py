bd15b0665d0d8e94cb293012856585ae98d03e48
Lucas Matheus <lukkasmatheu@gmail.com>
2023-06-25 16:42:11 -0300

    feat:Alterando script para reconhecer sistema

--- install.py
+++ install.py
@@ -16,64 +16,75 @@ def dowload_stack(nameFile):
         print("stack foi instalado com sucesso.") 
     except:
         print("Erro ao instalar o stack")
-
-    
-    if not os.path.exists("erd"):
-        print("Clonando o repositório Git...")
-        subprocess.run('git clone https://github.com/BurntSushi/erd.git', shell=True, check=True)
     # Executar o comando 'stack install' no diretório clonado
+    clone_erd()
     print("Executando stack install do projeto erd")
     try:
        subprocess.run(["C:\\Program Files\\stack\\stack.exe", 'install','--compiler=ghc-8.8.4','--resolver=lts-16.8'], shell=True, check=True, cwd="erd/")
     except:
         print("stack install concluído com sucesso.")
 
+def clone_erd():
+    if not os.path.exists("erd"):
+        print("Clonando o repositório Git...")
+        subprocess.run('git clone https://github.com/BurntSushi/erd.git', shell=True, check=True)
+    
+
 # Fazer o download do primeiro arquivo
-file1_path = os.path.expandvars("%APPDATA%\Roaming\local\bin\erd.exe")
-try:
-    if not os.path.exists(file1_path):
-        dowload_stack(file1_path)
-        print("Realização da instalação do erg-go finalizada com sucesso")
-    # Executar o primeiro arquivo
-except:
-    print("Não foi possivel baixar o erd")
-# Fazer o download do segundo arquivo
-file2_path = os.path.join("C:\\Program Files\\Graphviz\\bin", "dot.exe")
-pathSetting = os.path.join("PATH=%PATH%",file2_path)
-if not os.path.exists(file2_path):
+def install_windows():
+    file1_path = os.path.expandvars("%APPDATA%\Roaming\local\bin\erd.exe")
     try:
-        subprocess.run(['winget', 'install','-e', '--id' ,'Graphviz.Graphviz'],shell=True, check = True)
-        subprocess.run(["set", pathSetting], shell=True , check= True)
-        print("Instalação do graphviz realizada com sucesso")
-    except: 
-        print("Erro ao realizar a instalação do graphviz")
-# Atualizar o arquivo settings.json
-settings_file = os.path.expandvars(r"%APPDATA%\Code\User\settings.json")
+        if not os.path.exists(file1_path):
+            dowload_stack(file1_path)
+            print("Realização da instalação do erg-go finalizada com sucesso")
+        # Executar o primeiro arquivo
+    except:
+        print("Não foi possivel baixar o erd")
+    # Fazer o download do segundo arquivo
+    file2_path = os.path.join("C:\\Program Files\\Graphviz\\bin", "dot.exe")
+    pathSetting = os.path.join("PATH=%PATH%",file2_path)
+    if not os.path.exists(file2_path):
+        try:
+            subprocess.run(['winget', 'install','-e', '--id' ,'Graphviz.Graphviz'],shell=True, check = True)
+            subprocess.run(["set", pathSetting], shell=True , check= True)
+            print("Instalação do graphviz realizada com sucesso")
+        except: 
+            print("Erro ao realizar a instalação do graphviz")
+    # Atualizar o arquivo settings.json
+    settings_file = os.path.expandvars(r"%APPDATA%\Code\User\settings.json")
 
-# Ler o conteúdo atual do arquivo settings.json
-if os.path.exists(settings_file):
     # Ler o conteúdo atual do arquivo settings.json
-    with open(settings_file) as file:
-        settings = json.load(file)
+    if os.path.exists(settings_file):
+        # Ler o conteúdo atual do arquivo settings.json
+        with open(settings_file) as file:
+            settings = json.load(file)
 
-    # Adicionar as chaves e os caminhos dos arquivos
-    settings["erd-preview.erdPath"] = file1_path
-    settings["erd-preview.dotPath"] = file2_path
+        # Adicionar as chaves e os caminhos dos arquivos
+        settings["erd-preview.erdPath"] = file1_path
+        settings["erd-preview.dotPath"] = file2_path
 
-    # Escrever as alterações de volta no arquivo settings.json
-    with open(settings_file, "w") as file:
-        json.dump(settings, file, indent=4)
+        # Escrever as alterações de volta no arquivo settings.json
+        with open(settings_file, "w") as file:
+            json.dump(settings, file, indent=4)
 
-    # Informar que a atualização foi concluída
+        # Informar que a atualização foi concluída
+        print("Arquivo settings.json atualizado com sucesso!")
+    else:
+        print("O arquivo settings.json não existe.")
     print("Arquivo settings.json atualizado com sucesso!")
-else:
-    print("O arquivo settings.json não existe.")
-print("Arquivo settings.json atualizado com sucesso!")
+
+def install_linux():
+    subprocess.run(["sudo", "apt", "install", "graphviz"])
+    subprocess.run(["curl","-sSL","https://get.haskellstack.org/" +"|"+ "sh"])
+    clone_erd()
+    subprocess(["stack", "install", "--compiler=ghc-8.8.4","--resolver=lts-16.8"],cwd="erd/")
 
 # Verificar o sistema operacional atual
 if os.name == 'nt':  # Windows
+    install_windows()
     vscode_path = shutil.which('code.cmd')
 elif os.name == 'posix':  # Linux ou macOS
+    install_linux()
     vscode_path = shutil.which('code')
 else:
     print("Sistema operacional não suportado.")
